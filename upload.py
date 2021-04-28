4faaaad86fbdb7d2499a881294682f53a2dc5b47
cyberalchemist <cyberalchemist@makeralchemy.com>
2021-04-27 19:51:39 -0700

    initial commit

---
+++ upload.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect, url_for, abort
+import os
+
+app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024  # 10 MB
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
+app.config['UPLOAD_PATH'] = 'uploads'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    for uploaded_file in request.files.getlist('file'):
+        if uploaded_file.filename != '':
+            file_ext = os.path.splitext(uploaded_file.filename)[1]
+            if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+                abort(400)
+            uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], uploaded_file.filename))
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True, port=9000, host='0.0.0.0')
+
